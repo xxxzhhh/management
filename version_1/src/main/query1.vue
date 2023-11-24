8ef3fb07926bbde7e3950cec59cb45463b37127d
xxxzhhh <90552022+xxxzhhh@users.noreply.github.com>
2023-11-24 23:23:54 +0800

    Update query1.vue

--- version_1/src/main/query1.vue
+++ version_1/src/main/query1.vue
@@ -1,15 +1,18 @@
 <template>
   
     <el-row :gutter="12" >
-      
       <el-col :span="8">
-        <el-card shadow="hover" class="btn1" @click="toclass"> 班级成绩查看 </el-card>
+        <el-card shadow="hover" class="btn2" @click="tosubject"> 学科成绩查看 </el-card>
       </el-col>
       <el-col :span="8">
         <el-card shadow="hover" class="btn2" @click="tostudnet"> 学生成绩查看 </el-card>
       </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover" class="btn1" @click="toclass"> 班级成绩分析 </el-card>
+      </el-col>
+
     </el-row>
-    <div class="tip">Tips:若查看班级的整体成绩，点击“班级成绩查看”按钮；若查看某个学生的成绩状况，点击“学生成绩查看”按钮</div>
+    <div class="tip">Tips:若查看自己所教授学科的成绩情况，点击“学科成绩查看”；若查看某个学生的成绩状况，点击“学生成绩查看”按钮若查看班级的整体成绩，点击“班级成绩分析”按钮；</div>
   
   </template>
   <script>
@@ -19,9 +22,14 @@
           this.$router.push('/teacher/watchclass');
           console.log("班级成绩查看按钮被点击");
         },
+        tosubject() {
+          this.$router.push('/teacher/watchsubject');
+          console.log("学科成绩查看按钮被点击");
+        },
         tostudnet() {
           this.$router.push('/teacher/watchstudent');
           console.log("学生成绩查看按钮被点击");
+        tosubject() {
         },
       },
     };
